37c3bb885ce2ea2b0de6035f414dfad4b13ef520
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-04-05 18:21:26 +0200

    adding addWalletListener function

--- src/Minter.js
+++ src/Minter.js
@@ -28,6 +28,30 @@ const Minter = (props) => {
   const onMintPressed = async () => { //TODO: implement
     
   };
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   return (
     <div className="Minter">
