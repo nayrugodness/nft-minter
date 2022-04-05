0153924dce5131473635294271c3727f6f8abb0c
Nayru Ramirez <nanualexandraramirez@gmail.com>
2022-04-05 19:59:36 +0200

    Final mintNft function

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -87,52 +87,56 @@ export const getCurrentWalletConnected = async () => {
   };
 
   export const mintNFT = async(url, name, description) => {
+
     //error handling
     if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) { 
-           return {
-               success: false,
-               status: "❗Please make sure all fields are completed before minting.",
-           }
-     }
-   
-     //make metadata
-     const metadata = new Object();
-     metadata.name = name;
-     metadata.image = url;
-     metadata.description = description;
-   
-     //make pinata call
-     const pinataResponse = await pinJSONToIPFS(metadata);
-     if (!pinataResponse.success) {
-         return {
-             success: false,
-             status: "😢 Something went wrong while uploading your tokenURI.",
-         }
-     } 
-     const tokenURI = pinataResponse.pinataUrl;  
-            //set up your Ethereum transaction
-        const transactionParameters = {
-          to: contractAddress, // Required except during contract publications.
-          from: window.ethereum.selectedAddress, // must match user's active address.
-          'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI()//make call to NFT smart contract 
-        };
+        return {
+            success: false,
+            status: "❗Please make sure all fields are completed before minting.",
+        }
+    }
 
-        //sign the transaction via Metamask
-        try {
+    //make metadata
+    const metadata = new Object();
+    metadata.name = name;
+    metadata.image = url;
+    metadata.description = description;
+
+    //pinata pin request
+    const pinataResponse = await pinJSONToIPFS(metadata);
+    if (!pinataResponse.success) {
+        return {
+            success: false,
+            status: "😢 Something went wrong while uploading your tokenURI.",
+        }
+    } 
+    const tokenURI = pinataResponse.pinataUrl;  
+
+    //load smart contract
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
+
+    //set up your Ethereum transaction
+    const transactionParameters = {
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI() //make call to NFT smart contract 
+    };
+
+    //sign transaction via Metamask
+    try {
         const txHash = await window.ethereum
-          .request({
-              method: 'eth_sendTransaction',
-              params: [transactionParameters],
-          });
+            .request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters],
+            });
         return {
-          success: true,
-          status: "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash
+            success: true,
+            status: "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash
         }
-        } catch (error) {
+    } catch (error) {
         return {
-          success: false,
-          status: "😥 Something went wrong: " + error.message
+            success: false,
+            status: "😥 Something went wrong: " + error.message
         }
-
-        }
-   }
+    }
+}
